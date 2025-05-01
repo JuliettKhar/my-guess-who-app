a95d24baf0544ea00345d027053c0ebcf99021d2
julia kharlamova <juliettkhar@gmail.com>
2025-05-01 17:15:47 +0900

    resolve conflicts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 export default {
-    content: [
-        './src/**/*.{js,jsx,ts,tsx}',
-        './index.html',
-    ],
-    theme: {
-        extend: {}
-    },
-    plugins: []
-}
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
