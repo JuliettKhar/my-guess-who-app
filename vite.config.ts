ee53fa1cd77480f1858adc9e40f57a0bbf2e1693
julia kharlamova <juliettkhar@gmail.com>
2025-05-01 18:09:50 +0900

    add .dotenv

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
 import { fileURLToPath, URL } from 'node:url';
+import dotenv from 'dotenv';
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
