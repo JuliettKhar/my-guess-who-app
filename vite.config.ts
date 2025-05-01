fc8659777a0f796ffd51b322a749e30969158efc
julia kharlamova <juliettkhar@gmail.com>
2025-05-01 12:14:52 +0900

    update pages issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import tailwindcss from '@tailwindcss/vite';
+import { fileURLToPath, URL } from 'node:url';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/my-guess-who-app/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  plugins: [vue(), tailwindcss(),],
-})
+  plugins: [vue(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'index.js', // 💡 JS в корень
+        chunkFileNames: 'chunks/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
+});
